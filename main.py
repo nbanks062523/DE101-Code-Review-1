5a55d50c77a8f34742740448dcebbfd7519eba83
nbanks062523 <nbanks@kingsleytechpartners.com>
2023-10-11 22:29:59 -0400

    add puppies to main python file

---
+++ main.py
@@ -0,0 +1,18 @@
+# Welcome to our puppy barn !
+## Please take a moment to review our inventory
+
+## Below is a "grab-bag" list of different data types (float,string and integer)
+grab_bag= [.5,"I love puppies!",15]
+grab_bag
+
+## Now for a little list fun!
+print("My name is Nikki and",grab_bag[1])
+
+## This is a dictionary of our puppies and their ages
+puppies_dict= {"flash":1,"Chloe":6,"Kiki":20}
+print(puppies_dict)
+
+
+
+
+
